5568e1492bfe8cc687f273e55a092dc84ec2e4f0
greatsong <greatsong21@gmail.com>
2016-02-08 17:09:05 +0900

    Initial Commit

---
+++ 2016algorithmstudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2016algorithmstudy
+//
+//  Created by greatsong on 2/8/16.
+//  Copyright © 2016 greatsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
