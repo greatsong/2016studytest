197621fce6f6246fbf425ea7e066bab3392422ef
greatsong <greatsong21@gmail.com>
2016-02-17 01:49:38 +0900

    첫번째 깃허브 테스트용 커밋

---
+++ 2016testtest/2016testtest/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2016testtest
+//
+//  Created by greatsong on 2/17/16.
+//  Copyright © 2016 greatsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World?\n";
+    //1. commit : 어떤 수정이 있었는지를 기록
+    //2. push : 깃허브로 내용을 전송
+    return 0;
+}
